149af49fc72506a803aa7e57f333a81f487121e2
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-02-28 20:26:06 -0600

    Create discord.js

---
+++ discord/discord.js
@@ -0,0 +1,17 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const embedCfg = require('../../embedCfg.json')
+
+module.exports = {
+    cooldown: 5,
+    data: new SlashCommandBuilder()
+    .setName('discord')
+    .setDescription('gives main discord inv'),
+
+    async execute(interaction) {
+        const embed = new EmbedBuilder()
+        .setColor(embedCfg.color)
+        .setDescription('Our discord is: `https://discord.gg/au2JmPs3U6`')
+        
+        await interaction.reply({ embeds: [embed] })
+    }
+}
