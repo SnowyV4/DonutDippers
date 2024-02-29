2e531150ef8731ef1cd2bf4a3f81aaa1b920204b
snowy <97677273+SnowyV4@users.noreply.github.com>
2024-02-28 20:25:22 -0600

    Create consoleLog.js

---
+++ events/consoleLog.js
@@ -0,0 +1,10 @@
+const { Events, ActivityType } = require("discord.js");
+
+module.exports = {
+  name: Events.ClientReady,
+  once: true,
+  execute(client) {
+    console.log(`Discord bot ready \x1b[32m${client.user.username}\x1b[0m`);
+    client.user.setActivity("Donut SMP!", { type: ActivityType.Playing });
+  },
+};
